43de8d6e270013172b2f416e73edab4eec8736ea
Ilija Ivanov <ilija_ivanov@brown.edu>
2024-12-13 00:00:37 -0500

    implement client->server websocket commands, general project structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"crypto/rand"
 	"database/sql"
 	"encoding/base64"
@@ -13,6 +14,9 @@ import (
 	"net/http"
 	"net/mail"
 	"os"
+	"sync"
+
+	"time"
 
 	// "net/websocket"
 	"github.com/coder/websocket"
@@ -37,7 +41,14 @@ type registerRequest struct {
 	Username string `json:"username"`
 }
 
+type userRequest struct {
+	Type string `json:"type"` // types are: message, edit, delete, chanelSub, channelAdd, channelDelete
+	Arg1 string `json:"arg1"`
+	Arg2 string `json:"arg2,omitempty"`
+}
+
 var db *sql.DB
+var tokens sync.Map // maps token to user ID
 
 /*
 * API ENDPOINT HANDLER
@@ -99,13 +110,67 @@ func login(w http.ResponseWriter, req *http.Request) {
 }
 
 func handleWs(w http.ResponseWriter, req *http.Request) {
+	token := req.PathValue("token")
+	uidAny, ok := tokens.Load(token)
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("{\"error\": \"Invalid authentication token supplied\"}"))
+		return
+	}
+	userID := uidAny.(string)
 	c, err := websocket.Accept(w, req, nil)
 	if err != nil {
 		return
 	}
-	defer c.CloseNow()
-	ctx := c.CloseRead(req.Context())
-	<-ctx.Done()
+
+	go func() {
+		for {
+			_, msg, err := c.Read(context.Background())
+			if err != nil {
+				return
+			}
+			var userReq userRequest
+			parseErr := json.Unmarshal(msg, &userReq)
+			if parseErr != nil {
+				continue
+			}
+
+			arg1 := ""
+			switch userReq.Type {
+			case "message":
+				arg1, err = createMessage(userID, userReq.Arg1, userReq.Arg2)
+			case "edit":
+				err = editMessage(userID, userReq.Arg1, userReq.Arg2)
+			case "delete":
+				err = deleteMessage(userID, userReq.Arg1)
+			case "channelSub":
+				err = subscribeToChannel(userID, userReq.Arg1, userReq.Arg2)
+			case "channelAdd":
+				arg1, err = createChannel(userID, userReq.Arg1)
+			case "channelDelete":
+				err = deleteChannel(userID, userReq.Arg1)
+			default:
+				continue
+			}
+
+			ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*5)
+			response := make(map[string]string)
+			if err == nil {
+				response["result"] = "success"
+				if arg1 != "" {
+					response["arg1"] = arg1
+				}
+				marshaledResponse, _ := json.Marshal(response)
+				c.Write(ctx, websocket.MessageText, marshaledResponse)
+			} else {
+				response["result"] = "error"
+				response["message"] = err.Error()
+				marshaledResponse, _ := json.Marshal(response)
+				c.Write(ctx, websocket.MessageText, marshaledResponse)
+			}
+			cancelFunc()
+		}
+	}()
 }
 
 func createDatabase() {
@@ -165,10 +230,10 @@ func loginUser(email string, password string) (string, error) {
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public/auth/")))
-	http.Handle("/chat", http.FileServer(http.Dir("./public/chat/")))
+	http.Handle("/chat/", http.FileServer(http.Dir("./public/")))
 	http.HandleFunc("/api/login", login)
 	http.HandleFunc("/api/register", register)
-	http.HandleFunc("/ws", handleWs)
+	http.HandleFunc("/ws/{token}", handleWs)
 
 	setup := false
 	if _, err := os.Stat("database.db"); errors.Is(err, os.ErrNotExist) {
@@ -192,16 +257,43 @@ func main() {
 			log.Fatal(err.Error())
 		}
 
-		createTokenTableSQL := `CREATE TABLE token (
-			"uuid" TEXT NOT NULL PRIMARY KEY,		
-			"token" TEXT NOT NULL UNIQUE
+		createChannelsTableSQL := `CREATE TABLE channels (
+			"uuid" TEXT NOT NULL PRIMARY KEY,
+			"owner" TEXT NOT NULL,
+			"name" TEXT NOT NULL
 		);`
 
-		_, err = db.Exec(createTokenTableSQL)
+		_, err = db.Exec(createChannelsTableSQL)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		createMembershipTableSQL := `CREATE TABLE membership (
+			"channelUuid" TEXT NOT NULL,		
+			"userUuid" TEXT NOT NULL,
+			UNIQUE("channelUuid", "userUuid")
+		);`
+
+		_, err = db.Exec(createMembershipTableSQL)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		createMessagesTableSQL := `CREATE TABLE messages (
+			"msgUuid" TEXT NOT NULL PRIMARY KEY,
+			"timestamp" DATETIME NOT NULL,
+			"channelUuid" TEXT NOT NULL,		
+			"userUuid" TEXT NOT NULL,
+			"msg" TEXT NOT NULL
+		);`
+
+		_, err = db.Exec(createMessagesTableSQL)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
 	}
+
+	//* populate local channels
 
 	// start server at end because of blocking
 	err := http.ListenAndServe(":80", nil)
@@ -211,6 +303,141 @@ func main() {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+func createMessage(userUuid string, channelId string, content string) (string, error) {
+	//* verify that uuid is subscribed to channel
+	verifyMembershipSQL := "SELECT 1 FROM membership WHERE channelUuid = ? AND userUuid = ?;"
+
+	val := db.QueryRow(verifyMembershipSQL, channelId, userUuid)
+	exists := 1
+	err := val.Scan(&exists)
+	if err != nil {
+		return "", err
+	}
+
+	messageId := uuid.New().String()
+
+	addMessageSQL := `INSERT INTO messages ("msgUuid", "timestamp", "channelUuid", "userUuid", "msg") VALUES (?, ?, ?, ?, ?);`
+	_, err = db.Exec(addMessageSQL, messageId, time.Now(), channelId, userUuid, content)
+	if err != nil {
+		return "", err
+	}
+
+	//go sendMessage(messageId)
+	return messageId, nil
+}
+
+func editMessage(userUuid string, messageId string, content string) error {
+	verifyOwnershipSQL := "SELECT 1 FROM messages WHERE userUuid = ? AND msgUuid = ?;"
+
+	val := db.QueryRow(verifyOwnershipSQL, userUuid, messageId)
+	exists := 1
+	err := val.Scan(&exists)
+	if err != nil {
+		return err
+	}
+
+	editMessageSQL := `UPDATE messages SET msg = ? WHERE msgUuid = ?;`
+	_, err = db.Exec(editMessageSQL, content, messageId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteMessage(userUuid string, messageId string) error {
+	//* verify that uuid wrote the message
+	verifyOwnershipSQL := "SELECT 1 FROM messages WHERE userUuid = ? AND msgUuid = ?;"
+
+	val := db.QueryRow(verifyOwnershipSQL, userUuid, messageId)
+	exists := 1
+	err := val.Scan(&exists)
+	if err != nil {
+		return err
+	}
+
+	deleteMessageSQL := `DELETE FROM messages WHERE msgUuid = ?;`
+	_, err = db.Exec(deleteMessageSQL, messageId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func subscribeToChannel(userUuid string, channelId string, userToBeSubscribed string) error {
+	verifyMembershipSQL := "SELECT 1 FROM membership WHERE channelUuid = ? AND userUuid = ?;"
+
+	val := db.QueryRow(verifyMembershipSQL, channelId, userUuid)
+	exists := 1
+	err := val.Scan(&exists)
+	if err != nil {
+		return err
+	}
+
+	updateMembershipSQL := "INSERT OR REPLACE INTO membership (channelUuid, userUuid) VALUES (?, ?);"
+
+	_, err = db.Exec(updateMembershipSQL, channelId, userToBeSubscribed)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createChannel(userUuid string, name string) (string, error) {
+	createChannelSQL := `INSERT INTO channels (
+			"uuid",
+			"owner",
+			"name"
+		) VALUES (?, ?, ?);`
+
+	channelUuid := uuid.New().String()
+	_, err := db.Exec(createChannelSQL, channelUuid, userUuid, name)
+	if err != nil {
+		return "", err
+	}
+
+	insertMembershipSQL := `INSERT INTO membership (
+			"channelUuid",
+			"userUuid"
+		) VALUES (?, ?);`
+
+	_, err = db.Exec(insertMembershipSQL, channelUuid, userUuid)
+	if err != nil {
+		return "", err
+	}
+	return channelUuid, nil
+}
+
+func deleteChannel(userUuid string, channelId string) error {
+	//* verify that uuid created the channel
+	verifyOwnershipSQL := "SELECT 1 FROM channels WHERE uuid = ? AND owner = ?;"
+
+	val := db.QueryRow(verifyOwnershipSQL, channelId, userUuid)
+	exists := 1
+	err := val.Scan(&exists)
+	if err != nil {
+		return err
+	}
+
+	deleteChannelSQL := "DELETE FROM channels WHERE uuid = ?;"
+	_, err = db.Exec(deleteChannelSQL, channelId)
+	if err != nil {
+		return err
+	}
+
+	deleteMembershipSQL := "DELETE FROM membership WHERE channelUuid = ?;"
+	_, err = db.Exec(deleteMembershipSQL, channelId)
+	if err != nil {
+		return err
+	}
+
+	deleteMessagesSQL := "DELETE FROM messages WHERE channelUuid = ?;"
+	_, err = db.Exec(deleteMessagesSQL, channelId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 /*
@@ -223,12 +450,7 @@ func createSessionToken(uuid string) string {
 	}
 	// Encode to Base64 for URL-safe string
 	token := base64.URLEncoding.EncodeToString(randomBytes)
-	updateTokenSQL := "INSERT OR REPLACE INTO token (uuid, token) VALUES (?, ?)"
-
-	_, err := db.Exec(updateTokenSQL, uuid, token)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	tokens.Store(token, uuid)
 
 	return token
 }
@@ -240,6 +462,13 @@ func setCookieHandler(w http.ResponseWriter, token string) {
 	}
 
 	http.SetCookie(w, &cookie)
+}
+
+// ! potential race condition??
+// ! (user joins and fetches all messsages for channel after thread for msg1 gets all the users for channel
+// ! but before db is updated for msg2)
+func updateUsers(action string, arg1 string, arg2 string) {
+
 }
 
 // func getCookie(w http.ResponseWriter, r *http.Request) (string, error) {
